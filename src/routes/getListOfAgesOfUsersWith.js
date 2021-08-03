3c36aee0b0010ef2e05238679edfe897a3e60d56
shijie1xu <43320663+shijie1xu@users.noreply.github.com>
2021-08-03 12:06:44 -0400

    finish the project

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -1,10 +1,16 @@
 'use strict';
 const mockDBCalls = require('../database/index.js');
 
-const getListOfAgesOfUsersWithHandler = async (request, response) => {
-    const itemToLookup = 'carrot';
-    const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
-    return response.status(200).send(JSON.stringify(data));
+const getListOfAgesOfUsersWithHandler = async (request, response, next) => {
+    try{
+        const itemToLookup = request.query.item;
+        // const itemToLookup = 'carrot';
+        const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
+        return response.status(200).send(JSON.stringify(data));
+    }
+    catch (err){
+        return next(err);
+    }
 };
 
 module.exports = (app) => {
--- src/routes/getUsers.js
+++ src/routes/getUsers.js
@@ -1,9 +1,14 @@
 'use strict';
 const mockDBCalls = require('../database/index.js');
 
-const getUsersHandler = async (request, response) => {
-    const data = await mockDBCalls.getUsers();
-    return response.status(200).send(JSON.stringify(data));
+const getUsersHandler = async (request, response, next) => {
+    try{
+        const data = await mockDBCalls.getUsers();
+        return response.status(200).send(JSON.stringify(data));
+    }
+    catch (err){
+        return next(err);
+    }
 };
 
 module.exports = (app) => {
